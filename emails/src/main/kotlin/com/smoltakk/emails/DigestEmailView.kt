b08f78f1f1b2c0f6679c3f40398d6d7526e67bdd
Cara Esten Hurtle <dondeesten@gmail.com>
2020-08-01 12:37:23 -0700

    Add kotlin html DSL

--- emails/src/main/kotlin/com/smoltakk/emails/DigestEmailView.kt
+++ emails/src/main/kotlin/com/smoltakk/emails/DigestEmailView.kt
@@ -1,12 +1,24 @@
 package com.smoltakk.emails
 
 import com.smoltakk.models.Topic
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.html
+import kotlinx.html.stream.appendHTML
+import kotlinx.html.stream.createHTML
 
 class DigestEmailView(private val topics: List<Topic>) : EmailView {
     override val subject = "Yr daily TALK digest!"
 
-    override fun renderHtml(): String {
-        TODO("Not yet implemented")
+    override fun renderHtml() = buildString {
+        appendln("<!DOCTYPE html>")
+        appendHTML().html {
+            body {
+                h1 {
+                    text("TALK email")
+                }
+            }
+        }
     }
 
     override fun renderText(): String {
